44104ea683832517add50222e3f90771b22487d6
7Caylon7 <caylon.costa@itec.ufpa.br>
2024-08-14 20:20:08 -0300

    Setando variáveis de ambiente e limpando o que for desnecessário

---
+++ portfolio/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Adicione o atributo "escopo" para limitar o CSS apenas a este componente -->
+<style scoped>
+
+</style>
